6c9b69048cf90b49c6988a2d64b3197f3c5ff61d
codewithrajendra <41782748+codewithrajendra@users.noreply.github.com>
2019-10-18 22:58:48 +0530

    feat: Setup Home screen with recyclerview.

--- app/src/main/java/rajendra/gojekassignment/MainActivity.java
+++ app/src/main/java/rajendra/gojekassignment/MainActivity.java
@@ -1,6 +1,7 @@
 package rajendra.gojekassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
