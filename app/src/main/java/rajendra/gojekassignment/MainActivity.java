724f6f614b8d2f22fb68884eb1e7a3f369997ce8
codewithrajendra <41782748+codewithrajendra@users.noreply.github.com>
2019-10-19 00:26:20 +0530

    Fix: popup menu.

--- app/src/main/java/rajendra/gojekassignment/MainActivity.java
+++ app/src/main/java/rajendra/gojekassignment/MainActivity.java
@@ -1,18 +1,51 @@
 package rajendra.gojekassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        menu= (ImageButton) findViewById(R.id.menu);
 
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, menu);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.menu_main, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+
+        });
 
     }
+
+
 }
